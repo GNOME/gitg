c076c9cd1b22b9f4b732f8921116168910823978
Jesse van den Kieboom <jesse@icecrew.nl>
2009-01-30 09:41:39 +0100

    Fixed warning about non const format string

--- gitg/gitg-commit-view.c
+++ gitg/gitg-commit-view.c
@@ -1010,7 +1010,7 @@ get_comment(GitgCommitView *view)
 static void
 show_error(GitgCommitView *view, gchar const *error)
 {
-	GtkWidget *dlg = gtk_message_dialog_new(GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view))), GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, error);
+	GtkWidget *dlg = gtk_message_dialog_new(GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view))), GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, "%s", error);
 
 	gtk_dialog_run(GTK_DIALOG(dlg));
 	gtk_widget_destroy(dlg);
