32840505d38e0fb8a7fca18d0bbff81300331a91
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2010-11-06 14:53:26 +0100

    Added gpl

--- libgitg/gitg-line-parser.h
+++ libgitg/gitg-line-parser.h
@@ -1,3 +1,25 @@
+/*
+ * gitg-line-parser.h
+ * This file is part of gitg
+ *
+ * Copyright (C) 2010 - Jesse van den Kieboom
+ *
+ * gitg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * gitg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gitg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+
 #ifndef __GITG_LINE_PARSER_H__
 #define __GITG_LINE_PARSER_H__
 
