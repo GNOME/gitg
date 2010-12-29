2a322097eae7208ecff82f2d4bb80f52d57eaf43
Ignacio Casal Quinteiro <icq@gnome.org>
2010-12-29 15:51:19 +0100

    Use G_DEFINE_INTERFACE

--- gitg/gitg-activatable.c
+++ gitg/gitg-activatable.c
@@ -1,35 +1,28 @@
+/*
+ * gitg-activatable.c
+ * This file is part of gitg - git repository viewer
+ *
+ * Copyright (C) 2009 - Jesse van den Kieboom
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #include "gitg-activatable.h"
 
-static void gitg_activatable_default_init (GitgActivatableInterface *iface);
-
-GType
-gitg_activatable_get_type ()
-{
-	static GType gitg_activatable_type_id = 0;
-	
-	if (!gitg_activatable_type_id)
-	{
-		static const GTypeInfo g_define_type_info =
-		{
-			sizeof (GitgActivatableInterface),
-			(GBaseInitFunc) gitg_activatable_default_init,
-			NULL,
-			NULL,
-			NULL,
-			NULL,
-			0,
-			0,
-			NULL
-		};
-		
-		gitg_activatable_type_id = g_type_register_static (G_TYPE_INTERFACE,
-		                                                  "GitgActivatable",
-		                                                  &g_define_type_info,
-		                                                  0);
-	}
-
-	return gitg_activatable_type_id;
-}
+G_DEFINE_INTERFACE(GitgActivatable, gitg_activatable, G_TYPE_OBJECT)
 
 /* Default implementation */
 static gchar *
@@ -40,7 +33,7 @@ gitg_activatable_get_id_default (GitgActivatable *panel)
 
 static gboolean
 gitg_activatable_activate_default (GitgActivatable *panel,
-				      gchar const       *cmd)
+                                   gchar const       *cmd)
 {
 	return FALSE;
 }
@@ -69,7 +62,7 @@ gitg_activatable_get_id (GitgActivatable *panel)
 
 gboolean
 gitg_activatable_activate (GitgActivatable *panel,
-			   gchar const     *action)
+                           gchar const     *action)
 {
 	g_return_val_if_fail (GITG_IS_ACTIVATABLE (panel), FALSE);
 
