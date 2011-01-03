018b7d5af519f5859d670bbef64c96291df2c6cc
Garrett Regier <alias301@gmail.com>
2011-01-02 20:59:45 -0800

    Set GtkAboutDialog's license-type instead of our own license.

--- gitg/gitg-window.c
+++ gitg/gitg-window.c
@@ -2212,21 +2212,6 @@ on_help_about (GtkAction  *action,
 	static gchar const copyright[] = "Copyright \xc2\xa9 2009 Jesse van den Kieboom";
 	static gchar const *authors[] = {"Jesse van den Kieboom <jessevdk@gnome.org>", NULL};
 	static gchar const *comments = N_ ("gitg is a git repository viewer for gtk+/GNOME");
-	static gchar const *license = N_ ("This program is free software; you can redistribute it and/or modify\n"
-		"it under the terms of the GNU General Public License as published by\n"
-		"the Free Software Foundation; either version 2 of the License, or\n"
-		"(at your option) any later version.\n"
-		"\n"
-		"This program is distributed in the hope that it will be useful,\n"
-		"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-		"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-		"GNU General Public License for more details.\n"
-		"\n"
-		"You should have received a copy of the GNU General Public License\n"
-		"along with this program; if not, write to the Free Software\n"
-		"Foundation, Inc., 59 Temple Place, Suite 330,\n"
-		"Boston, MA 02111-1307, USA.");
-
 
 	gchar *path = gitg_dirs_get_data_filename ("icons", "gitg.svg", NULL);
 	GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file (path, NULL);
@@ -2247,7 +2232,7 @@ on_help_about (GtkAction  *action,
 	                       "website", "http://trac.novowork.com/gitg",
 	                       "website-label", "trac.novowork.com/gitg",
 	                       "logo", pixbuf,
-	                       "license", _ (license),
+	                       "license-type", GTK_LICENSE_GPL_2_0,
 	                       NULL);
 
 	if (pixbuf)
