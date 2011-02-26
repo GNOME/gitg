cdeef6ffd7e77f82bcf3434f3fb40f7cbdc45b00
Javier Jardón <jjardon@gnome.org>
2011-02-26 14:39:41 +0000

    build: Update autotools configuration

--- autogen.sh
+++ autogen.sh
@@ -19,4 +19,4 @@ which gnome-autogen.sh || {
     exit 1
 }
 
-ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I m4" REQUIRED_AUTOMAKE_VERSION=1.8 USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+. gnome-autogen.sh
