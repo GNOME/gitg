c11657dc9a5170c63e62c5195c73a5cad1838fc7
Jesse van den Kieboom <jessevdk@gmail.com>
2014-01-04 15:16:04 +0100

    Use non-recursive make to build plugins

--- icons.mk
+++ icons.mk
@@ -1,8 +1,8 @@
-baseicondir = $(datadir)/gitg/icons/hicolor
-svgicondir = $(baseicondir)/scalable/actions
-svgicon_DATA = $(ICONS)
+baseiconsdir = $(datadir)/gitg/icons/hicolor
+scalableiconsdir = $(baseiconsdir)/scalable/actions
+scalableicons_DATA = $(SCALABLE_ICONS)
 
-gtk_update_icon_cache = $(GTK_UPDATE_ICON_CACHE) -f -t $(baseicondir)
+gtk_update_icon_cache = $(GTK_UPDATE_ICON_CACHE) -f -t $(baseiconsdir)
 
 install-data-hook: update-icon-cache
 uninstall-hook: update-icon-cache
@@ -16,5 +16,4 @@ update-icon-cache:
 		echo "***   $(gtk_update_icon_cache)"; \
 	fi
 
-EXTRA_DIST = \
-	$(svgicon_DATA)
+EXTRA_DIST += $(scalableicons_DATA)
