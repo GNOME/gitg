1e78dab056a0e694bbf3558d588dcc7ef5233f5d
Ignacio Casal Quinteiro <icq@gnome.org>
2015-12-19 23:03:02 +0100

    Add missing copyright and modeline

--- libgitg/gitg-platform-support-osx.c
+++ libgitg/gitg-platform-support-osx.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of gitg
+ *
+ * Copyright (C) 2015 - Jesse van den Kieboom
+ *
+ * gitg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * gitg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gitg. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "gitg-platform-support.h"
 
 #include <gdk/gdkquartz.h>
@@ -65,3 +84,5 @@ gitg_platform_support_http_get_finish (GAsyncResult  *result,
 {
 	return g_task_propagate_pointer (G_TASK (result), error);
 }
+
+// ex:ts=4 noet
