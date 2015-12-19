2fe0726b4c7308d4f14990538b88948a03bf7c49
Ignacio Casal Quinteiro <icq@gnome.org>
2015-12-19 23:02:01 +0100

    Fix build

--- libgitg/gitg-platform-support.c
+++ libgitg/gitg-platform-support.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of gitg
+ *
+ * Copyright (C) 2015 - Jesse van den Kieboom
+ *
+ * gitg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * gitg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gitg. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "gitg-platform-support.h"
 
 gboolean
@@ -19,5 +38,7 @@ GInputStream *
 gitg_platform_support_http_get_finish (GAsyncResult  *result,
                                        GError       **error)
 {
-	return G_INPUT_STREAM (g_file_read_async_finish (g_async_result_get_source_object (result), result, error);
+	return G_INPUT_STREAM (g_file_read_finish (g_async_result_get_source_object (result), result, error));
 }
+
+// ex:ts=4 noet
--- libgitg/gitg-platform-support.h
+++ libgitg/gitg-platform-support.h
@@ -1,7 +1,27 @@
+/*
+ * This file is part of gitg
+ *
+ * Copyright (C) 2015 - Jesse van den Kieboom
+ *
+ * gitg is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * gitg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gitg. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __GITG_PLATFORM_SUPPORT_H__
 #define __GITG_PLATFORM_SUPPORT_H__
 
 #include <gdk/gdk.h>
+#include <gio/gio.h>
 
 gboolean gitg_platform_support_use_native_window_controls (GdkDisplay *display);
 
@@ -15,3 +35,4 @@ GInputStream *gitg_platform_support_http_get_finish (GAsyncResult         *resul
 
 #endif /* __GITG_PLATFORM_SUPPORT_H__ */
 
+// ex:ts=4 noet
