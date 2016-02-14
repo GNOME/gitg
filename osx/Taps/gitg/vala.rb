555295777643d1b02eb3b1c38192500261860fff
Jesse van den Kieboom <jessevdk@gnome.org>
2016-02-14 11:59:31 +0100

    [osx] Update vala dependency

---
+++ osx/Taps/gitg/vala.rb
@@ -0,0 +1,51 @@
+class Vala < Formula
+  desc "Compiler for the GObject type system"
+  homepage "https://live.gnome.org/Vala"
+  url "https://download.gnome.org/sources/vala/0.31/vala-0.31.1.tar.xz"
+  sha256 "c3cbff1cc29f3fca3939c3994b8591ec3d9aca4d85d042353ee46c00ddf4055f"
+
+  bottle do
+    sha256 "b68681f676381035cdd85e944aa234f0566137bda514ec105477fbeed246a365" => :el_capitan
+    sha256 "752fe91460002b335c35084f073d6e3f3666017f6b7dea9f728a828a0dde911e" => :yosemite
+    sha256 "bf422cf5802d09413482943afac7cd0b7d19e2defa1955330acb11d644d61299" => :mavericks
+  end
+
+  devel do
+    url "https://download.gnome.org/sources/vala/0.31/vala-0.31.1.tar.xz"
+    sha256 "c3cbff1cc29f3fca3939c3994b8591ec3d9aca4d85d042353ee46c00ddf4055f"
+  end
+
+  depends_on "pkg-config" => :run
+  depends_on "gettext"
+  depends_on "glib"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make" # Fails to compile as a single step
+    system "make", "install"
+  end
+
+  test do
+    test_string = "Hello Homebrew\n"
+    path = testpath/"hello.vala"
+    path.write <<-EOS
+      void main () {
+        print ("#{test_string}");
+      }
+    EOS
+    valac_args = [ # Build with debugging symbols.
+      "-g",
+      # Use Homebrew's default C compiler.
+      "--cc=#{ENV.cc}",
+      # Save generated C source code.
+      "--save-temps",
+      # Vala source code path.
+      path.to_s]
+    system "#{bin}/valac", *valac_args
+    assert File.exist?(testpath/"hello.c")
+
+    assert_equal test_string, shell_output("#{testpath}/hello")
+  end
+end
