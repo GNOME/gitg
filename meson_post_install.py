c876e0e255f23dea2b04e6bfc5233f4e1ce0f685
Iñigo Martínez <inigomartinez@gmail.com>
2017-10-02 16:01:18 +0200

    build: Port to meson build system
    meson is a build system focused on speed an ease of use, which
    helps speeding up the software development. This patch adds meson
    support along autotools.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=788796

---
+++ meson_post_install.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import re
+import subprocess
+import sys
+
+if not os.environ.get('DESTDIR'):
+  icondir = os.path.join(sys.argv[1], 'icons', 'hicolor')
+
+  name_pattern = re.compile('hicolor_(?:apps)_(?:\d+x\d+|scalable)_(.*)')
+  search_pattern = '/**/hicolor_*'
+
+  [os.rename(file, os.path.join(os.path.dirname(file), name_pattern.search(file).group(1)))
+   for file in glob.glob(icondir + search_pattern, recursive=True)]
+
+  print('Update icon cache...')
+  subprocess.call(['gtk-update-icon-cache', '-f', '-t', '-q', icondir])
+
+  icondir = os.path.join(sys.argv[1], 'gitg', 'icons', 'hicolor')
+  subprocess.call(['gtk-update-icon-cache', '-f', '-t', '-q', icondir])
+
+  schemadir = os.path.join(sys.argv[1], 'glib-2.0', 'schemas')
+  print('Compiling gsettings schemas...')
+  subprocess.call(['glib-compile-schemas', schemadir])
+
+  if sys.argv[2] == 'enable-python':
+    print('Byte-compiling python modules...')
+    subprocess.call(['python', '-m', 'compileall', '-f', '-q', sys.argv[3]])
+
+    print('Byte-compiling python modules (optimized versions) ...')
+    subprocess.call(['python', '-O', '-m', 'compileall', '-f', '-q', sys.argv[3]])
