88a5090465931e97ecb52531022d6ced06793538
Alberto Fanjul <albertofanjul@gmail.com>
2018-10-25 16:09:40 +0200

    Fixes on windows installer

--- win32/make-installer.bat
+++ win32/make-installer.bat
@@ -1,9 +1,16 @@
+@echo off
+
 C:/msys64/usr/bin/bash.exe -c "./make-installer stage1"
 if errorlevel 1 (
 exit /b %errorlevel%
 )
 
 C:/msys64/usr/bin/bash.exe -c "./make-installer stage2"
+if errorlevel 1 (
+exit /b %errorlevel%
+)
+
+C:/msys64/usr/bin/bash.exe -c "./make-installer stage3"
 if errorlevel 1 (
 exit /b %errorlevel%
 )
